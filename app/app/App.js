6b4e5dca55d3515c7b773c661f95909d424143de
Kiara Jacob <kiarajacob03@gmail.com>
2022-03-05 13:02:05 +0530

    updated header bar

--- app/app/App.js
+++ app/app/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // added to avoid overlap with the iphone notch
+    <SafeAreaView> 
+      <Header/>
+    </SafeAreaView>
   );
 }
 
---
+++ app/app/components/User.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function user() {
+  return (
+    <View>
+      <Text>USER</Text>
+    </View>
+  )
+}
