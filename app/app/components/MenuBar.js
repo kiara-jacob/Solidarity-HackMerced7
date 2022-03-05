8b413b25f31dd941b7a9f21e7d63b98f3c1021a0
Kiara Jacob <kiarajacob03@gmail.com>
2022-03-05 13:01:44 +0530

    Co-authored-by: Kianna Ng <kiannang@users.noreply.github.com>

---
+++ app/app/components/MenuBar.js
@@ -0,0 +1,13 @@
+import {
+  Dropdown }
+  from 'react-native-material-dropdown';
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function MenuBar() {
+  return (
+    <View>
+      <Text>MenuBar</Text>
+    </View>
+  )
+}
